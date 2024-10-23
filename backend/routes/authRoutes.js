6ad113033932c615acc806a57116a7074f0fd041
srikriydv <srikri5462ydv@gmail.com>
2024-10-24 01:08:16 +0545

    auth: resend verification email, password reset and logout controllers

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,6 +4,13 @@ import verifyUserEmail from "../controllers/auth/verifyEmailController.js";
 import loginUser from "../controllers/auth/loginController.js";
 import { loginLimiter } from "../middleware/apiLimiter.js";
 import newAccessToken from "../controllers/auth/refreshTokenController.js";
+import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
+import logoutUser from "../controllers/auth/logoutController.js";
+import {
+	resetPassword,
+	resetPasswordRequest,
+} from "../controllers/auth/passwordResetController.js";
+
 
 const router = express.Router();
 
@@ -14,5 +21,13 @@ router.get("/verify/:emailToken/:userId", verifyUserEmail);
 router.post("/login", loginLimiter, loginUser);
 
 router.get("/new_access_token", newAccessToken);
+
+router.post("/resend_email_token", resendEmailVerificationToken);
+
+router.post("/reset_password_request", resetPasswordRequest);
+
+router.post("/reset_password", resetPassword);
+
+router.get("/logout", logoutUser);
 
 export default router;
