c770bd1213f2c57e637b7c6a89dfa38d0d45ff70
srikriydv <srikri5462ydv@gmail.com>
2024-10-24 19:47:09 +0545

    Homepage and other components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,30 @@
-import Footer from "./components/Footer"
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import Footer from "./components/Footer";
+import Layout from "./components/Layout";
+import NotFound from "./components/NotFound";
+import { customTheme } from "./customTheme";
+import useTitle from "./hooks/useTitle";
+import HomePage from "./pages/HomePage";
 
-function App() {
+const App = () => {
+	useTitle("MERN Invoice - Home");
+	return (
+		<ThemeProvider theme={customTheme}>
+			<CssBaseline />
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<HomePage />} />
+					<Route path="*" element={<NotFound />} />
+				</Route>
+			</Routes>
+			<Footer />
+			<ToastContainer theme="dark" />
+		</ThemeProvider>
+	);
+};
 
-  return (
-    <>
-      <h1>Hello everyone</h1>
-      <Footer />
-    </>
-  )
-}
-
-export default App
+export default App;
